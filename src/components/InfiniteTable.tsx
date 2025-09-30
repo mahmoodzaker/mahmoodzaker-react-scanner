5864b79a74cccccb4764fc9c8a118a0a0d98e1fd
mahmoodzaker <mahmoodzaker@gmail.com>
2025-10-01 01:22:59 +0330

    Subscribe to ws

--- src/components/InfiniteTable.tsx
+++ src/components/InfiniteTable.tsx
@@ -1,11 +1,12 @@
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 import { AutoSizer, GridCellProps, Index, MultiGrid } from 'react-virtualized'
-import { ScannerResult } from '../types/test-task-types'
+import { chainIdToName, ScannerResult } from '../types/test-task-types'
 
 const ROW_HEIGHT = 50
-const COLUMN_WIDTH = [250, 200]
+const COLUMN_WIDTH = [250, 500, 200, 300, 200, 200, 200, 200, 200]
 type InfiniteTableProps = {
   data: ScannerResult[]
+  page: number
 }
 
 function RowSpan({
@@ -16,17 +17,16 @@ function RowSpan({
   children: ReactNode
 }) {
   return (
-    <span style={{ width: `${COLUMN_WIDTH[columnIndex]}px` }}>{children}</span>
+    <span
+      className="flex flex-col overflow-hidden px-1 w-full"
+      style={{ width: `${COLUMN_WIDTH[columnIndex]}px` }}
+    >
+      {children}
+    </span>
   )
 }
 export function InfiniteTable(props: InfiniteTableProps) {
-  const { data } = props
-  const [state] = useState({
-    fixedColumnCount: 1,
-    fixedRowCount: 0,
-    scrollToColumn: 0,
-    scrollToRow: 0,
-  })
+  const { data, page } = props
 
   const _cellRenderer = ({
     columnIndex,
@@ -36,16 +36,50 @@ export function InfiniteTable(props: InfiniteTableProps) {
   }: GridCellProps) => {
     const row = data[rowIndex]
     return (
-      <div key={key} style={style} className="border border-1 border-[#222222]">
+      <div
+        key={key}
+        style={style}
+        className="w-full border border-1 border-[#222222]"
+      >
         {columnIndex == 0 && (
           <RowSpan columnIndex={columnIndex}>
-            #{rowIndex + 1} {row.token0Symbol}/{row.token1Symbol}
+            <span>
+              #{(page - 1) * 100 + rowIndex + 1} {row.token1Name}
+            </span>
+            <span>
+              {row.token0Symbol}/{row.token1Symbol} {chainIdToName(row.chainId)}
+            </span>
           </RowSpan>
         )}
         {columnIndex == 1 && (
+          <RowSpan columnIndex={columnIndex}>{row.routerAddress}</RowSpan>
+        )}
+        {columnIndex == 2 && (
+          <RowSpan columnIndex={columnIndex}>{row.price}</RowSpan>
+        )}
+        {columnIndex == 3 && (
+          <RowSpan columnIndex={columnIndex}>market cap</RowSpan>
+        )}
+        {columnIndex == 4 && (
+          <RowSpan columnIndex={columnIndex}>{row.volume}</RowSpan>
+        )}
+        {columnIndex == 5 && (
+          <RowSpan columnIndex={columnIndex}>proce chnage</RowSpan>
+        )}
+        {columnIndex == 6 && (
+          <RowSpan columnIndex={columnIndex}>{row.age}</RowSpan>
+        )}
+        {columnIndex == 7 && (
           <RowSpan columnIndex={columnIndex}>
-            {row.price}
+            <span>{(row.buys || 0) + (row.sells || 0)}</span>
+            <span className="flex gap-1">
+              <span className="text-green-600">{row.buys || 0}</span>/
+              <span className="text-red-600">{row.sells || 0}</span>
+            </span>
           </RowSpan>
+        )}
+        {columnIndex == 8 && (
+          <RowSpan columnIndex={columnIndex}>{row.liquidity}</RowSpan>
         )}
       </div>
     )
@@ -56,7 +90,6 @@ export function InfiniteTable(props: InfiniteTableProps) {
       <AutoSizer>
         {({ width, height }) => (
           <MultiGrid
-            {...state}
             cellRenderer={_cellRenderer}
             columnWidth={(index: Index) => COLUMN_WIDTH[index.index] || 100}
             columnCount={COLUMN_WIDTH.length}
--- src/components/ReactScanner.tsx
+++ src/components/ReactScanner.tsx
@@ -1,24 +1,119 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { ScannerApiResponse } from '../types/test-task-types'
+import { useEffect, useRef, useState } from 'react'
+import {
+  chainIdToName,
+  GetScannerResultParams,
+  OutgoingWebSocketMessage,
+  ScannerApiResponse,
+} from '../types/test-task-types'
 import { InfiniteTable } from './InfiniteTable'
+const PAGE_SIZE = 100
 
 export function ReactScanner() {
+  const params: GetScannerResultParams = {}
   const [scanData, setScanData] = useState<ScannerApiResponse>()
+  const [scanCurrentPage, setScanCurrentPage] = useState<number>(1)
+  const [isConnected, setConnected] = useState(false)
+
   useEffect(() => {
+    setScanData(undefined)
     axios
-      .get('https://api-rs.dexcelerate.com/scanner')
+      .get('https://api-rs.dexcelerate.com/scanner', {
+        params,
+      })
       .then((response) => {
         setScanData(response.data)
       })
       .catch((error) => console.error(error))
+  }, [scanCurrentPage])
+
+  const sendMessage = (message: OutgoingWebSocketMessage) => {
+    if (!ws.current) return false
+    if (ws.current.readyState != WebSocket.OPEN) return false
+    ws.current.send(JSON.stringify(message))
+  }
+  const ws = useRef<WebSocket | null>(null)
+
+  useEffect(() => {
+    ws.current = new WebSocket('wss://api-rs.dexcelerate.com/ws')
+    const wsCurrent = ws.current
+    wsCurrent.onopen = () => {
+      console.log('opened')
+      setConnected(true)
+    }
+    wsCurrent.onclose = () => {
+      console.log('ws closed')
+      setConnected(false)
+    }
+    wsCurrent.onmessage = (e) => {
+      const message = JSON.parse(e.data)
+      console.log('e', message)
+    }
+    return () => {
+      wsCurrent.close()
+    }
   }, [])
 
-  if(!scanData) return <div>Loading...</div>
+  useEffect(() => {
+    if (!isConnected || !scanData?.pairs) return
+    const wsCurrent = ws.current
+    sendMessage({ event: 'scanner-filter', data: params })
+    for (const token of scanData.pairs) {
+      const tokenData = {
+        chain: chainIdToName(token.chainId),
+        pair: token.pairAddress,
+        token: token.token1Address,
+      }
+      sendMessage({
+        event: 'subscribe-pair',
+        data: tokenData,
+      })
+      sendMessage({
+        event: 'subscribe-pair-stats',
+        data: tokenData,
+      })
+    }
+
+    return () => {
+      sendMessage({ event: 'unsubscribe-scanner-filter', data: params })
+      for (const token of scanData.pairs) {
+        const tokenData = {
+          chain: chainIdToName(token.chainId),
+          pair: token.pairAddress,
+          token: token.token1Address,
+        }
+        sendMessage({
+          event: 'unsubscribe-pair',
+          data: tokenData,
+        })
+        sendMessage({
+          event: 'unsubscribe-pair-stats',
+          data: tokenData,
+        })
+      }
+    }
+  }, [scanData, isConnected])
+
+  if (!scanData) return <div>Loading...</div>
+  const pageCount = Math.ceil(scanData.totalRows / PAGE_SIZE)
+
   return (
     <div className="flex h-full w-full justify-between">
-      <InfiniteTable data={scanData.pairs} />
-      {/*<InfiniteTable />*/}
+      <div className="flex flex-col w-full h-full">
+        <InfiniteTable data={scanData.pairs} page={scanCurrentPage} />
+        <div>
+          Page:
+          <select
+            onChange={(e) => setScanCurrentPage(parseInt(e.target.value))}
+          >
+            {Array.from({ length: pageCount }, (v, i) => i).map((_, index) => (
+              <option selected={index == scanCurrentPage - 1} value={index + 1}>
+                {index + 1}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   )
 }
